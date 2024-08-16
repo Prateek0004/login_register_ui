4740d02a96a7e4f7d66322d14afde39a65a20ce4
UTKARSH UPADHYAY <pu9651155991@gmai.com>
2024-08-16 13:32:39 +0530

    Initial commit

---
+++ lib/WelcomeScreen.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+
+import 'RegScreen.dart';
+import 'loginScreen.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                colors: [
+                  Color(0xffB81736),
+                  Color(0xff281537),
+                ]
+            )
+        ),
+        child: Column(
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(top: 200.0),
+                child: Image(image: AssetImage('assets/logo.png')),
+              ),
+              const SizedBox(
+                height: 100,
+              ),
+              const Text('Welcome Back',style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.white
+              ),),
+              const SizedBox(height: 30,),
+              GestureDetector(
+                onTap: (){
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => const loginScreen()));
+                },
+                child: Container(
+                  height: 53,
+                  width: 320,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    border: Border.all(color: Colors.white),
+                  ),
+                  child: const Center(child: Text('SIGN IN',style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white
+                  ),),),
+                ),
+              ),
+              const SizedBox(height: 30,),
+              GestureDetector(
+                onTap: (){
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => const RegScreen()));
+                },
+                child: Container(
+                  height: 53,
+                  width: 320,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(30),
+                    border: Border.all(color: Colors.white),
+                  ),
+                  child: const Center(child: Text('SIGN UP',style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black
+                  ),),),
+                ),
+              ),
+              const Spacer(),
+              // const Text('Login with Other...',style: TextStyle(
+              //     fontSize: 17,
+              //     color: Colors.white
+              // ),),//
+              // const SizedBox(height: 12,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center, // To center the images horizontally
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 45.0),
+                    child: Image(
+                      image: AssetImage('assets/google.png'),
+                      height: 40,
+                      width: 40,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 30.0, bottom: 45.0), // Add some spacing between images if needed
+                    child: Image(
+                      image: AssetImage('assets/facebook.png'),
+                      height: 40,
+                      width: 40,
+                    ),
+                  ),
+                ],
+              )
+            ]
+        ),
+      ),
+
+    );
+  }
+}
